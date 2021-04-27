31d2e982097d72af94c190ca6256f0408e505726
Rita <hsen.yeh123@gmail.com>
2021-04-28 00:01:03 +0800

    fear:add create function

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// 引用 body-parser
+const bodyParser = require('body-parser')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //mongoDatabase
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -28,9 +33,23 @@ app.get('/', (req, res) => {
   Todo.find()//取出Todo Model所有資料
     .lean()// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(console.log('Error'))// 錯誤處理
+    .catch(error => console.log('Error'))// 錯誤處理
 
 })
+
+//Create
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+
 
 app.listen(port, () => {
   console.log('The server is running on http://localhost:3000')

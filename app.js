53fb6e9e600bc789c28bdd02fcdcc8dbbfbb74d7
Rita <hsen.yeh123@gmail.com>
2021-04-27 16:40:13 +0800

    feat- add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('Mongodb connected!')
+})
+
+
 app.get('/', (req, res) => {
   res.send('This is the website.')
 

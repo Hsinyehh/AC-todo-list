a901ba1ba9b02a599ffef900e4110556ce9b5847
Rita <hsen.yeh123@gmail.com>
2021-04-27 23:31:32 +0800

    feat: add seeders and handlebars

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+//handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//mongoDatabase
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -14,10 +21,10 @@ db.once('open', () => {
   console.log('Mongodb connected!')
 })
 
-
+// 設定路由
+// Todo 首頁
 app.get('/', (req, res) => {
-  res.send('This is the website.')
-
+  res.render('index')
 })
 
 app.listen(port, () => {

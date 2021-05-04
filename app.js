c4159c87008fe8f3c2bea88378d7e375dff5e622
Rita <hsen.yeh123@gmail.com>
2021-05-04 23:04:32 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')//載入method override
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -16,21 +16,10 @@ const bodyParser = require('body-parser')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//mongoDatabase
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 // 設定每筆要求都會透過methodOverride進行前置處理
 app.use(methodOverride('_method'))
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('Mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('Mongodb connected!')
-})
 
 app.use(routes)
 
---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose') // 載入 mongoose
+
+
+//mongoDatabase
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('Mongodb connected!')
+})
+
+module.exports = db
